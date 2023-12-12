cc31049858df5ba2fb42426f19400126308deb5d
Kieran <khjgames@gmail.com>
2023-12-12 13:52:11 -0500

    cool

--- GameObjects.cpp
+++ GameObjects.cpp
@@ -32,6 +32,34 @@ RectObject& GameObjectsCollection::GetRect(string name) {
 	}
 }
 
+struct Gate {
+	Vector2 Position;
+	Gate(GameObjectsCollection SceneObjects){ 
+		Position = Vector2();
+		RectObject& Rect_1 = SceneObjects.CreateRect();
+			Rect_1.Filled = false;
+			Rect_1.Color.Set(0, 50, 5);
+			Rect_1.Size.Set(17, 17);
+			Rect_1.Position.Set(Position.x, Position.y);
+		RectObject& Rect_2 = SceneObjects.CreateRect();
+			Rect_2.Filled = false;
+			Rect_2.Color.Set(0, 50, 5);
+			Rect_2.Size.Set(50, 4);
+			Rect_2.Position.Set(Position.x+17, Position.y+13);
+		RectObject& Rect_3 = SceneObjects.CreateRect();
+			Rect_3.Filled = false;
+			Rect_3.Color.Set(0, 43, 74);
+			Rect_3.Size.Set(17, 17);
+			Rect_3.Position.Set(Position.x+67, Position.y);
+		RectObject& Rect_4 = SceneObjects.CreateRect();
+			Rect_4.Filled = false;
+			Rect_4.Color.Set(0, 43, 74);
+			Rect_4.Size.Set(50, 4);
+			Rect_4.Position.Set(Position.x + 17 + 67, Position.y + 13);
+	}; // constructor
+	~Gate(){}; // destructor
+};
+
 void GameObjectsCollection::Render(){
 	for (unsigned i = 0; i < RectObjects.size(); ++i){
 		RectObjects[i].Draw(CameraPosition);
